f193729f754d4a3851776308c8ea4d9cbee27eea
alessio <alessiodg99.adg@gmail.com>
2020-11-12 15:44:00 +0100

    fine domande

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+//variabili domande
+var nome = prompt("Qual è il tuo nome?");
+var cognome = prompt("Qual è il tuo cognome?");
+var colore = prompt("Qual è il tuo colore preferito?");
+
+//password
+document.getElementById("password").innerHTML = (nome + cognome + colore + "19")
